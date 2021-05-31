094da7a83bd0adb4141b7789d02811e424bfe1cd
Tyson-Xwort <84491720+Tyson-xwort@users.noreply.github.com>
2021-05-31 11:25:50 +0530

    loop.py

---
+++ loop.py
@@ -0,0 +1,6 @@
+1. compute first 10 natural number using while loop:
+i = 0       # i = 0 means index started from zero.
+while(i <= 10):
+    print(i,end =" ")      # end = " " --> means it will print in horizontal line.
+    i = i +1
+    
